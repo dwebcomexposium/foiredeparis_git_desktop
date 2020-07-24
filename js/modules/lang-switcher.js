ca405f0c417fb57622e7c5de20c91fc7d0ffe812
dwebcomexposium <dweb@comexposium.com>
2020-07-24 11:26:40 +0200

    Add logic to handle multiple langages

--- js/modules/lang-switcher.js
+++ js/modules/lang-switcher.js
@@ -8,24 +8,33 @@
  */
 
 (function ($) {
-  function updateLangSwitcher(switcher) {
-    $('link[rel="alternate"][hreflang]').each(function () {
-      const lang = $(this).attr("hreflang");
-      const url = $(this).attr("href");
-      const link = switcher.find(`a[lang="${lang}"]`);
-      const frHrefLink = link.context.children[1].children[0].children[0];
-      const gbHrefLink = link.context.children[1].children[1].children[0];
-      if (lang === "fr") {
-        gbHrefLink.href = url;
-      } else {
-        frHrefLink.href = url;
-      }
-    });
-  }
-
   $(".js-lang-switcher")
     .each(function () {
-      var $_this = $(this);
+      function updateLangSwitcher(switcher) {
+        $('link[rel="alternate"][hreflang]').each(function () {
+          const lang = $(this).attr("hreflang");
+          const url = $(this).attr("href");
+          const link = switcher.find(`a[lang="${lang}"]`);
+          if (link) {
+            link.map(function(){ return this.href = url})
+          }
+        });
+      }
+
+      let $_this = $(this);
+
+      let target = document.querySelector("title");
+      let observer = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
+          setTimeout(function () {
+            updateLangSwitcher($_this);
+          }, 500);
+        });
+      });
+
+      let config = {
+        childList: true,
+      };
 
       // button creation
       $_this
@@ -43,24 +52,30 @@
         $_this.find(".js-toggle-trigger").trigger("click");
       });
 
-      // update lang switcher url when title change for comexposium connect 2
-      let target = document.querySelector("title");
-      let observer = new MutationObserver(function (mutations) {
-        mutations.forEach(function (mutation) {
-          setTimeout(function () {
-            updateLangSwitcher($_this);
-          }, 500);
-        });
+      // Allows lang switcher to be displayed above "Back to top" if opened
+      // It happens in mobile where lang-switcher is in footer
+      // When lang switcher is closed, we still want it to be displayed under "Back to top"
+      $_this.find(".js-toggle-trigger").on("click.ls", function () {
+        var $lsToggle = $(this);
+        // Adding higher z-index to footer > .inside than to its sibling .is-stuck.sf-ttt
+        // It seems .is-open isn't there yet when the test is made so z-index is set in the 'else' statement
+        let z = $(".is-stuck.sf-ttt").css("zIndex");
+        if ($_this.hasClass("is-open")) {
+          $(".site-footer .inside").css({
+            zIndex: "auto",
+          });
+        } else {
+          $(".site-footer .inside").css({
+            zIndex: z + 1,
+          });
+        }
+        return false;
       });
 
-      let config = {
-        childList: true,
-      };
-
+      // update lang switcher url when title change for comexposium connect 2
       observer.observe(target, config);
-      setTimeout(function () {
-        updateLangSwitcher($_this);
-      }, 500);
+
+      updateLangSwitcher($_this);
     })
     .toggleSlide();
 })(jQuery);
